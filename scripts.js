9e60260a6f54a6da789bb60728dd99922ac9501b
Vanesa Paola Soria <94073289+vanehit@users.noreply.github.com>
2023-02-13 19:39:49 -0300

    ejercicos basicos javascript

---
+++ scripts.js
@@ -0,0 +1,105 @@
+console.log('Hola Mundo!')
+
+/*--------------------EJERCICIO 1 AL 6----------------------*/
+
+//let miNombre = "Vanesa";
+//let miApellido = "Soria";
+//let miEdad = 33;
+//let miMascota ="Tita";
+//let edadMascota = 7;
+
+
+//console.log(miNombre)
+//console.log(miApellido)
+
+
+/*alert(miEdad)*/
+/*alert(miMascota)*/
+/*alert(edadMascota)*/
+
+/*------------------EJERCICO 7----------*/
+
+//let miNombre = "Vanesa";
+//let miApellido = "Soria";
+
+//let nombreCompleto = miNombre + " " + miApellido 
+
+//console.log(nombreCompleto)
+
+/*--------------EJERCICIO 8-------------*/
+
+//let textoPresentacion = miNombre + " " + miApellido + " " +  miEdad + " " + miMascota + " " + edadMascota
+
+//console.log(textoPresentacion)
+
+/*--------------EJERCICIO 9-------------*/
+
+/*let miEdad = 33;
+let edadMascota = 7;
+
+let sumaEdades = 33 + 7
+
+console.log(sumaEdades)*/
+
+//document.write(sumaEdades)
+
+/*let miEdad = 33;
+let edadMascota = 7;
+
+let restaEdades = 33 - 7
+
+console.log(restaEdades)*/
+
+
+/*let miEdad = 33;
+let edadMascota = 7;
+
+let productoEdades = 33 * 7
+
+console.log(productoEdades)*/
+
+/*let miEdad = 33;
+let edadMascota = 7;
+
+let divisionEdades = 33 / 7
+
+console.log(divisionEdades)*/
+
+
+
+
+
+/*--------------EJERCICIO 10-------------*/
+
+//let nombreIngresado = prompt('Ingrese su nombre:')
+    //console.log(nombreIngresado)
+
+//let apellidoIngresado = prompt('Ingrese su apellido:')
+    //console.log(apellidoIngresado)
+
+//let edadIngresada = prompt('Ingrese su edad:')
+    //console.log(edadIngresada)
+
+//let mascotaIngresada = prompt('Ingrese nombre de su mascota:')
+    //console.log(mascotaIngresada)
+
+//let edadMascota = prompt('Ingrese edad de su mascota:')
+    //console.log(edadMascota)
+
+/*--------------EJERCICIO 11-------------*/
+
+let alumno = {
+    nombre:"Vanesa",
+    edad: 33,
+    estudiante: "true",
+    curso: "JavaScript",
+    mascota: "Tita"
+}
+
+console.table(alumno)
+
+//console.log(alumno.nombre)
+//console.log(alumno.edad)
+//console.log(alumno.estudiante)
+//console.log(alumno.curso)
+//console.log(alumno.mascota)
